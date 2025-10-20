2cc755ecfdbf6a49c3d0c072d5816245a17136b3
Chris Anzilotti <anzilottichr@gmail.com>
2025-10-20 01:35:09 -0400

    Added Features to Complex Frame Boundary

--- DatasetTool.py
+++ DatasetTool.py
@@ -2,6 +2,7 @@ import os
 from locale import normalize
 from pathlib import Path
 from scipy.signal import butter, filtfilt
+from sklearn.metrics.pairwise import cosine_similarity
 
 import librosa
 import numpy as np
@@ -31,9 +32,57 @@ class SongLoader:
     def get_songs(self):
         return self.songs
 
+
+
+
+
 def get_phrase_boundaries_complex(songs):
     y, sr = preprocessing(songs)
-    pass
+
+    #Features
+    chroma = compute_chroma(y, sr)
+    mfcc = compute_mfcc(y, sr)
+    tempogram, onset_env = compute_tempogram(y, sr)
+    features = np.concatenate([chroma, mfcc, tempogram, onset_env], axis=0)
+
+    # Part comes from Chatgpt because
+    # I couldn't fully understand how to
+    # recreate Foote's design
+    SSM[i, j] = cosine_similarity(features.T)
+
+
+
+
+
+# Detects the beat/tempo structure
+# The background beat that is played
+# Also detects onset which is when the musical note begins
+def compute_tempogram(y, sr):
+    onset_env = librosa.onset.onset_strength(y=y, sr=sr)
+    tempogram = librosa.feature.tempogram(onset_envelope=onset_env, sr=sr)
+    tempogram = librosa.util.normalize(tempogram)
+    onset_env = librosa.util.normalize(onset_env)
+    return tempogram, onset_env
+
+# Computes the timbre (texture or tone color)
+# The unique sounds of the instruments
+# Helps detect sudden changes in instruments or vocals
+def compute_mfcc(y, sr):
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+    timbre = librosa.feature.delta(mfcc)
+    mfcc_features = np.concatenate((mfcc, timbre))
+    librosa.util.normalize(mfcc_features, axis=1)
+    return mfcc_features
+
+
+# Extracts the Chroma which is the quality of pitch
+# Used to be categorized into one of twelve distinct pitch classes
+def compute_chroma(y, sr):
+    S = np.abs(librosa.stft(y))
+    chroma = librosa.feature.chroma_stft(S, sr=sr)
+    chroma = librosa.decompose.nn_filter(chroma)
+    chroma = librosa.util.normalize(chroma, azis=0)
+    return chroma
 
 
 def preprocessing(songs):
@@ -48,7 +97,7 @@ def preprocessing(songs):
 def highpass_filter(y, sr, cutoff=100.0):
     b, a = butter(N=2,  Wn=cutoff / (sr / 2.0), btype='high', analog=False)
     y = filtfilt(b, a, y)
-
+    return y
 
 
 
