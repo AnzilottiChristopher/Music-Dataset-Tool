cc9954f010a44d20d873fc81f3e01d3fedbb3da6
Chris Anzilotti <anzilottichr@gmail.com>
2025-11-10 15:51:54 -0500

    Git Ignore + Trend Line Stuff

--- WriteToJson.py
+++ WriteToJson.py
@@ -19,22 +19,21 @@ class writeToJson:
             raise ValueError(f"Data does not exist. Please input the data: {attr_name}")
 
     def create_file(self, path=None, data=None):
-        data = self.resolve(data, 'data')
-        path = self.resolve(path, 'path')
-
+        data = self.resolve(data, "data")
+        path = self.resolve(path, "path")
 
         try:
-            with open(path, 'w') as f:
-                json.dump(self.data, f)
+            with open(path, "w") as f:
+                json.dump(self.data, f, ensure_ascii="utf-8")
         except:
             print("Error writing to file")
 
     def add_entry(self, data, path=None):
-        path = self.resolve(path, 'path')
+        path = self.resolve(path, "path")
 
         # Load existing data or create a new structure
         try:
-            with open(path, 'r') as f:
+            with open(path, "r") as f:
                 self.data = json.load(f)
         except (FileNotFoundError, json.JSONDecodeError):
             self.data = {"songs": []}
@@ -47,26 +46,24 @@ class writeToJson:
         self.data["songs"].extend(data["songs"])
 
         # Write updated JSON
-        with open(path, 'w') as f:
-            json.dump(self.data, f, indent=4, ensure_ascii=False)
+        with open(path, "w") as f:
+            json.dump(self.data, f, indent=4, ensure_ascii="utf-8")
 
     def append_entry(self, data, entry, path=None):
-        path = self.resolve(path, 'path')
-        song_name = data['song_name']
-        with open(path, 'r+') as i:
+        path = self.resolve(path, "path")
+        song_name = data["song_name"]
+        with open(path, "r+") as i:
             try:
                 self.data = json.load(i)
             except:
                 print("file not found")
 
-        if entry.lower() == 'song_name'.lower():
+        if entry.lower() == "song_name".lower():
             for i in self.data:
-                if i['song_name'].lower() == song_name.lower():
-                    self.data[0]['song_name'] = data[0]['song_name']
+                if i["song_name"].lower() == song_name.lower():
+                    self.data[0]["song_name"] = data[0]["song_name"]
         else:
             return
 
-        with open(self.path, 'w') as f:
+        with open(self.path, "w") as f:
             json.dump(self.data, f, indent=4, ensure_ascii=False)
-
-
